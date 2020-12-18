de4df8b8aa10e9d88437f7e66de0eb53d7a2cfaa
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2020-12-18 16:34:49 -0300

    test post method

--- spring-ess/src/main/java/com/matheusf/springess/controller/AnimeController.java
+++ spring-ess/src/main/java/com/matheusf/springess/controller/AnimeController.java
@@ -1,13 +1,17 @@
 package com.matheusf.springess.controller;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.matheusf.springess.domain.Anime;
 import com.matheusf.springess.service.AnimeService;
@@ -23,18 +27,23 @@ import lombok.extern.log4j.Log4j2;
 public class AnimeController {
 
 	private final DateUtil dateUtil;
-	private final AnimeService animalService;
+	private final AnimeService animeService;
 	
 	@GetMapping
 	public ResponseEntity<List<Anime>> list(){
 		log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-		return ResponseEntity.ok().body(animalService.listAll());
+		return ResponseEntity.ok().body(animeService.listAll());
 	}	
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Anime> findById(@PathVariable long id){
 		log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-		return ResponseEntity.ok().body(animalService.findById(id));
+		return ResponseEntity.ok().body(animeService.findById(id));	
+	}
 	
+	@PostMapping
+	public ResponseEntity<Anime> save(@RequestBody Anime anime){
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(anime.getId()).toUri();
+		return ResponseEntity.created(uri).body(animeService.save(anime));
 	}
 }
