e1177c7e52ec588febe395ba60cae76e1bc8feab
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2020-12-18 13:25:33 -0300

    first commit

---
+++ spring-ess/src/main/java/com/matheusf/springess/SpringEssApplication.java
@@ -0,0 +1,13 @@
+package com.matheusf.springess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEssApplication.class, args);
+	}
+
+}
---
+++ spring-ess/src/test/java/com/matheusf/springess/SpringEssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusf.springess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
