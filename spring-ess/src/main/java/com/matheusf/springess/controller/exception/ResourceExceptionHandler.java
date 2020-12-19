3200d17b031d28942f59c834af9eab5e3ce8c61e
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2020-12-19 20:15:33 -0300

    sobrescevendo handler do spring

--- spring-ess/src/main/java/com/matheusf/springess/controller/exception/ResourceExceptionHandler.java
+++ spring-ess/src/main/java/com/matheusf/springess/controller/exception/ResourceExceptionHandler.java
@@ -1,49 +1,60 @@
 package com.matheusf.springess.controller.exception;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.matheusf.springess.exception.BadRequestException;
 
 @ControllerAdvice
-public class ResourceExceptionHandler {
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(BadRequestException.class)
-	public ResponseEntity<StandardError> badRequest(BadRequestException e) {
-		StandardError see = StandardError.builder()
-							.timestamp(LocalDateTime.now())
-							.status(HttpStatus.BAD_REQUEST.value())
-							.title("Bad Request Exception, Check the Documentation")
-							.details(e.getMessage())
-							.developerMessage(e.getClass().getName())
-							.build();
+	public ResponseEntity<StandardError> handleBadRequest(BadRequestException e) {
+		StandardError see = StandardError.builder().timestamp(LocalDateTime.now())
+				.status(HttpStatus.BAD_REQUEST.value()).title("Bad Request Exception, Check the Documentation")
+				.details(e.getMessage()).developerMessage(e.getClass().getName()).build();
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(see);
-	}	
-	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ValidationError> validationError(MethodArgumentNotValidException e) {
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 		ValidationError see = ValidationError.builder()
-							.timestamp(LocalDateTime.now())
-							.status(HttpStatus.BAD_REQUEST.value())
-							.title("Bad Request Exception, Check the Documentation")
-							.details("Check the errors")
-							.developerMessage(e.getClass().getName())
-							.build();	
-		
-		see.initializeList();			
-		for (FieldError x : e.getBindingResult().getFieldErrors()) {				
-			see.addError(FieldMessage.builder()
-					.field(x.getField())
-					.message(x.getDefaultMessage())
-					.build());
+				.timestamp(LocalDateTime.now())
+				.status(HttpStatus.BAD_REQUEST.value())
+				.title("Bad Request Exception, Check the Documentation")
+				.details("Check the errors")
+				.developerMessage(ex.getClass().getName())
+				.build();
+
+		see.initializeList();
+		for (FieldError x : ex.getBindingResult().getFieldErrors()) {
+			see.addError(FieldMessage.builder().field(x.getField()).message(x.getDefaultMessage()).build());
 		}
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(see);							
-	}	
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(see);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		StandardError see = StandardError.builder()
+			.timestamp(LocalDateTime.now())
+			.status(status.value())
+			.title(ex.getCause().getMessage())
+			.details(ex.getMessage())
+			.developerMessage(ex.getClass().getName())
+			.build();
+
+		return new ResponseEntity<>(see, headers, status);
+	}
 }
