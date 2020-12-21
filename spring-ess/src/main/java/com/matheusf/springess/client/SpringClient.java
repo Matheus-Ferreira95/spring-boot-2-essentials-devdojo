a721d3465e56a8b6433c3b4590fe6871abf20189
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2020-12-21 16:41:26 -0300

    RestTemplate Post

--- spring-ess/src/main/java/com/matheusf/springess/client/SpringClient.java
+++ spring-ess/src/main/java/com/matheusf/springess/client/SpringClient.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -19,7 +22,8 @@ import lombok.extern.log4j.Log4j2;
 public class SpringClient implements CommandLineRunner {	
 		
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) throws Exception {		
+		
 		ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 1);
 		log.info(entity);		
 						
@@ -29,13 +33,26 @@ public class SpringClient implements CommandLineRunner {
 		Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
 		log.info(Arrays.toString(animes));
 		
-		ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all", HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>() {} );
-		log.info(exchange.getBody());
-			
+		ResponseEntity<List<Anime>> listAnimes = new RestTemplate().exchange("http://localhost:8080/animes/all", HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>() {} );
+		log.info(listAnimes.getBody());		
 		
-				
-				
+		/*
+		Anime kingdom = new Anime(null, "coragem o cao covarde");
+		Anime kingdomSaved = new RestTemplate().postForObject("http://localhost:8080/animes", kingdom, Anime.class);
+		log.info(kingdomSaved);
+		*/
+		
+		Anime yugioh = Anime.builder().name("yugioh").build();
+		ResponseEntity<Anime> kaiba = new RestTemplate().exchange("http://localhost:8080/animes", HttpMethod.POST, new HttpEntity<>(yugioh, createJsonHeader()),
+				new ParameterizedTypeReference<Anime>() {
+				});
 		
 	}	
+	
+	private static HttpHeaders createJsonHeader() {
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(MediaType.APPLICATION_JSON);
+		return header;
+	}
 	
 }
--- spring-ess/src/main/java/com/matheusf/springess/controller/exception/FieldMessage.java
+++ spring-ess/src/main/java/com/matheusf/springess/controller/exception/FieldMessage.java
@@ -1,12 +1,15 @@
 package com.matheusf.springess.controller.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class FieldMessage {
 	
 	private final String field;
 	private final String message;	
+	
 }
