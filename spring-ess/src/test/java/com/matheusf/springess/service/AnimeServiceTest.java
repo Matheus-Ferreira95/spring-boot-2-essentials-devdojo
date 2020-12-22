1d26f6282e6bedc6aea5f9a9ab92ae71fa08560a
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2020-12-22 17:56:47 -0300

    package service test

--- spring-ess/src/test/java/com/matheusf/springess/service/AnimeServiceTest.java
+++ spring-ess/src/test/java/com/matheusf/springess/service/AnimeServiceTest.java
@@ -1,4 +1,4 @@
-package com.matheusf.springess;
+package com.matheusf.springess.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
